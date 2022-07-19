043d1374747a27f9646d6d7450dea1f78fa2fd96
siddhant gulia <67831735+siddcyber@users.noreply.github.com>
2022-07-19 15:26:33 +0530

    changed method of downloading the zip file
    Need to extract to specific folder and delete

--- BhavCopyAutoDownloader.py
+++ BhavCopyAutoDownloader.py
@@ -2,7 +2,7 @@
 
 import os
 from datetime import date, datetime, timedelta
-from nsepy.history import get_price_list
+import requests
 
 # file opens after 5 minutes of startup, asks user to edit file (provide auto edit option, ask first time for auto edit)
 
@@ -14,6 +14,14 @@ from nsepy.history import get_price_list
 # saturday  -> friday   ->5
 # sunday    -> friday   ->6
 
+# nsepy method
+# from nsepy.history import get_price_list
+# yesterday = today - timedelta(days=1)  # yesterday's date
+# nameDateYesterday = nameDateToday - timedelta(days=1)  # Yesterday's date for file naming
+# prises = get_price_list(dt=yesterday)  # gets latest Bhav NSE copy list
+# # saving of file in CSV format with name
+# prises.to_csv(os.path.join(path, 'cm' + nameDateYesterday.strftime("%d%b%Y") + 'bhav.csv'))
+
 #  Add try except statements to check if internet works if not retry after 1 minute
 #  and convert code into class(if possible)
 with open((os.getcwd() + "\BhavCopy_location.txt"), 'r') as file:
@@ -22,23 +30,31 @@ with open((os.getcwd() + "\BhavCopy_location.txt"), 'r') as file:
 today = date.today()  # today's date
 nameDateToday = datetime.today()  # today's date for file naming
 
+
 if today.weekday() == 6:
-    yesterday = today - timedelta(days=2)
     nameDateYesterday = nameDateToday - timedelta(days=2)  # Friday's date for file naming
-    prises = get_price_list(dt=yesterday)  # gets latest Bhav NSE copy list
     # saving of file in CSV format with name
-    prises.to_csv(os.path.join(path, 'cm' + nameDateYesterday.strftime("%d%b%Y") + 'bhav.csv'))
 
 elif today.weekday() == 0:
-    yesterday = today - timedelta(days=3)
     nameDateYesterday = nameDateToday - timedelta(days=3)  # Friday's date for file naming
-    prises = get_price_list(dt=yesterday)  # gets latest Bhav NSE copy list
     # saving of file in CSV format with name
-    prises.to_csv(os.path.join(path, 'cm' + nameDateYesterday.strftime("%d%b%Y") + 'bhav.csv'))
 
 else:
-    yesterday = today - timedelta(days=1)  # yesterday's date
     nameDateYesterday = nameDateToday - timedelta(days=1)  # Yesterday's date for file naming
-    prises = get_price_list(dt=yesterday)  # gets latest Bhav NSE copy list
     # saving of file in CSV format with name
-    prises.to_csv(os.path.join(path, 'cm' + nameDateYesterday.strftime("%d%b%Y") + 'bhav.csv'))
+
+
+URL = "https://archives.nseindia.com/content/historical/EQUITIES/" + \
+      str(nameDateYesterday.strftime("%Y")+"/" + nameDateYesterday.strftime("%b").upper() + "/cm" +
+          nameDateYesterday.strftime("%d%b%Y").upper()) + 'bhav.csv.zip'
+
+
+# 2. download the data behind the URL
+response = requests.get(URL)
+
+# 3. Open the response into a new file called instagram.ico
+open(str("cm" + nameDateYesterday.strftime("%d%b%Y").upper() + 'bhav.csv.zip'), "wb").write(response.content)
+
+# extract zip file to specified location
+
+#  delete the zip file
