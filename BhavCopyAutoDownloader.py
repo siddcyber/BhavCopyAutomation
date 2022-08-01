6b474aa48721042a669085b9ec5d1abd02cf54dd
siddhant gulia <67831735+siddcyber@users.noreply.github.com>
2022-08-01 12:22:04 +0530

    added code to check if BhavCopy_location.txt is present Loops until file is found and retries after some time

--- BhavCopyAutoDownloader.py
+++ BhavCopyAutoDownloader.py
@@ -1,10 +1,11 @@
 # install packages: pywin32, pandas, requests, future, and winshell
-
+#  add try statement if bhavcopylocation not found, open BhavcopyAutoSettings and retry
 import os
 import requests
 from datetime import date, datetime, timedelta
 from zipfile import ZipFile
 import time
+
 # monday    -> friday   ->0
 # tuesday   -> monday   ->1
 # wednesday -> tuesday  ->2
@@ -12,11 +13,6 @@ import time
 # friday    -> thursday ->4
 # saturday  -> friday   ->5
 # sunday    -> friday   ->6
-
-# read the bhavcopy_location file for the save location of the NSE Bhavcopy
-with open((os.getcwd() + "\BhavCopy_location.txt"), 'r') as file:
-    path = str(file.readline())
-
 today = date.today()  # today's date
 nameDateToday = datetime.today()  # today's date for file naming
 
@@ -36,9 +32,17 @@ zipname = str("cm" + nameDateYesterday.strftime("%d%b%Y").upper() + 'bhav.csv.zi
 URL2 = "https://archives.nseindia.com/content/historical/EQUITIES/" + \
        str(nameDateYesterday.strftime("%Y") + "/" + nameDateYesterday.strftime("%b").upper()) + '/' + zipname
 
-# main function to repeat until the file is downloaded, unzipped and delete the zip file
 # while true method takes 0.7 to 0.19 seconds
 while True:
+    # read the bhavcopy_location file for the save location of the NSE Bhavcopy
+    try:
+        with open((os.getcwd() + r"\BhavCopy_location.txt"), 'r') as file:
+            path = str(file.readline())
+    except (FileNotFoundError):
+        os.startfile(os.getcwd() + r"\BhavCopyAutoSettings.exe")
+        time.sleep(60)
+
+    # main function to repeat until the file is downloaded, unzipped and delete the zip file
     try:
         response = requests.get(URL2)  # download the data behind the URL
         open(zipname, "wb").write(response.content)  # Open the response into a new file
@@ -47,5 +51,5 @@ while True:
             zip_file.extractall(path=path)
         os.remove(zipname)  # removes the downloaded zip file
         break
-    except (requests.exceptions.ConnectionError, FileNotFoundError):
+    except (requests.exceptions.ConnectionError, FileNotFoundError, NameError):
         time.sleep(10)
